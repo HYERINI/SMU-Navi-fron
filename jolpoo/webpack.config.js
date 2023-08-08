7607e9e994d4d68b4ad20268aea365663058b825
HYERINI <rinlee2001@naver.com>
2023-08-08 16:57:04 +0900

    배포

--- jolpoo/webpack.config.js
+++ jolpoo/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry : "./src/App.js",
+    entry : "./src/index.js",
     output : {
         path : path.resolve(__dirname, "bundleBuild"),
         filename : 'build.js'
